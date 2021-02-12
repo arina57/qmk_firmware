7e70a7909d43dc8b051e80f208470a01b9db91d8
Arina <arina_57@hotmail.co.jp>
2021-02-12 12:32:32 +0900

    added new keyboard

--- keyboards/crkbd_deisatus/keymaps/arina57/keymap.c
+++ keyboards/crkbd_deisatus/keymaps/arina57/keymap.c
@@ -18,7 +18,7 @@ uint8_t deadzoneMin[] = {-10, -10};
 char newline[2] = "\n";
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_split_3x6_3(
+    [0] = LAYOUT_split_3x6_4(
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         KC_ESC,    KC_Q,   KC_W,    KC_E,    KC_R,     KC_T,                         KC_Y,   KC_U,     KC_I,     KC_O,  KC_P,   KC_LALT,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -26,40 +26,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_LSFT,   KC_Z,   KC_X,    KC_C,    KC_V,     KC_B,                         KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_LCTL,
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                            KC_LGUI,  KC_SPC,   MO(1),                   MO(2),  KC_ENT,  KC_BSPC
+                            KC_LGUI,  KC_SPC,   MO(1),  KC_MS_BTN1,     KC_NO, MO(2),  KC_ENT,  KC_BSPC
                                         //`--------------------------'  `--------------------------'
         ),
 
-    [1] = LAYOUT_split_3x6_3(
+    [1] = LAYOUT_split_3x6_4(
 
         KC_HAEN, KC_1, KC_2, KC_3, KC_4, KC_5,                              KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_LPRN, KC_RPRN,                       KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_LT, KC_GT,                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LGUI,
-                                            KC_TRNS, KC_TRNS,               MO(3), KC_ENT, KC_DEL
+                                            KC_TRNS, KC_TRNS, KC_MS_BTN1,     KC_NO,          MO(3), KC_ENT, KC_DEL
         ),
 
-    [2] = LAYOUT_split_3x6_3(
+    [2] = LAYOUT_split_3x6_4(
 
         KC_HANJ, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_LBRC,KC_RBRC,                        KC_MINS, KC_EQL, KC_BSLS, KC_GRV, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_LCBR, KC_RCBR,                       KC_UNDS, KC_PLUS, KC_PIPE, KC_TILD, KC_BTN1, KC_BTN2,
-                                KC_LGUI, KC_SPC, MO(3),                 KC_TRNS, KC_TRNS, KC_NO)
+                                KC_LGUI, KC_SPC, MO(3),KC_MS_BTN1,     KC_NO,  KC_TRNS, KC_TRNS, KC_NO)
         ,
 
-    [3] = LAYOUT_split_3x6_3(
+    [3] = LAYOUT_split_3x6_4(
 
         KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                           KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
         RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, KC_NO, KC_NO,                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F12,
         RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, KC_NO, KC_NO,                   KC_NO, KC_NO, KC_NO, KC_NO, DF(0), DF(4),
-                            KC_LGUI, KC_SPC, KC_TRNS,                KC_TRNS, KC_ENT, KC_NO
+                            KC_LGUI, KC_SPC, KC_TRNS, KC_MS_BTN1,     KC_NO, KC_TRNS, KC_ENT, KC_NO
         ),
 
-    [4] = LAYOUT_split_3x6_3(
+    [4] = LAYOUT_split_3x6_4(
 
         KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,                               KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LALT,
         KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,                               KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                              KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_TRNS,
-                        KC_LCTL, KC_SPC, MO(1),                     MO(2), KC_ENT, KC_BSPC
+                        KC_LCTL, KC_SPC, MO(1), KC_MS_BTN1,     KC_NO, MO(2), KC_ENT, KC_BSPC
         )
         };
 
